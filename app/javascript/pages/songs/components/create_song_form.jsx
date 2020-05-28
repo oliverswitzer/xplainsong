8ae82f4f8778918e801188791f673ceaea8f0992
Oliver Switzer <oliverswitzer@gmail.com>
2020-05-28 14:31:06 -0400

    Some spring cleaning of how I structure components -- now all songs related components are within the pages/songs directory
    Also remove channels related js since i wont use that right now

--- app/javascript/pages/songs/components/create_song_form.jsx
+++ app/javascript/pages/songs/components/create_song_form.jsx
@@ -1,7 +1,7 @@
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import React, { useState } from "react";
-import { SongGateway } from "../gateways/song_gateway";
+import { SongGateway } from "../../../gateways/song_gateway";
 
 export const CreateSongForm = ({ onSubmit }) => {
   const [formState, setFormState] = useState({ title: '' });
--- app/javascript/pages/songs/index.jsx
+++ app/javascript/pages/songs/index.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-import { SongsList } from "../components/songs_list";
-import { CreateSongForm } from "../components/create_song_form";
+import { SongsList } from "./components/songs_list";
+import { CreateSongForm } from "./components/create_song_form";
 import useSWR from "swr";
-import { SongGateway } from "../gateways/song_gateway";
+import { SongGateway } from "../../gateways/song_gateway";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 
 
-export const SongsPage = () => {
+export default () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data: songs, mutate: refreshSongs } = useSWR(SongGateway.CACHE_KEYS.all, SongGateway.all);
