76e0b9c1104b024d7ee9b986e3ad1ae57f242a79
Oliver Switzer <oliverswitzer@gmail.com>
2020-05-29 16:51:03 -0400

    Update jest config to include no jsx files

--- jest.config.js
+++ jest.config.js
@@ -149,7 +149,7 @@ module.exports = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: ['.*test.js?(x)'],
+  testRegex: ['.*test.js(x)?'],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
