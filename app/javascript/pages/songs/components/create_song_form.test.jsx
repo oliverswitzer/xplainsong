a76d40173a8c491a27e2d35110ff54cc21ae863b
Oliver Switzer <oliverswitzer@gmail.com>
2020-05-28 22:48:27 -0400

    Add enzyme/jest along with a simple component test for CreateSongForm

---
+++ app/javascript/pages/songs/components/create_song_form.test.jsx
@@ -0,0 +1,32 @@
+import { SongGateway } from "../../../gateways/song_gateway";
+import { CreateSongForm } from "./create_song_form";
+import { shallow } from 'enzyme'
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+
+jest.mock('../../../gateways/song_gateway');
+
+describe('CreateSongForm', () => {
+  let componentWrapper;
+  let onSubmitSpy;
+  beforeEach(() => {
+    onSubmitSpy = jest.fn();
+    SongGateway.mockClear();
+    componentWrapper = shallow(<CreateSongForm onSubmit={onSubmitSpy}/>)
+
+    componentWrapper.find(TextField).simulate('change', { target: { value: 'Some title' } });
+    componentWrapper.find(Button).simulate('click');
+
+  });
+
+  describe('when form is filled and submitted', () => {
+    it('calls SongGateway.create with the song data', () => {
+      expect(SongGateway.create).toHaveBeenCalledWith({ title: 'Some title' })
+    });
+
+    it('calls the onSubmit callback', () => {
+      expect(onSubmitSpy).toHaveBeenCalledWith()
+    });
+  })
+});
